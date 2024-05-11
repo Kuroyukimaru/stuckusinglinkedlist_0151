22f52d9a8f848023f842d0c5f66d8c4da133956b
Kuroyukimaru <oranggabut712@gmail.com>
2024-05-11 10:11:39 +0700

    Masukkan nilai yang ingin dimasukkan

--- stuckusinglinkedlist_0151/stuckusinglinkedlist_0151.cpp
+++ stuckusinglinkedlist_0151/stuckusinglinkedlist_0151.cpp
@@ -76,6 +76,12 @@ int main() {
         cout << "4. exit\n";
         cin >> choice;
 
-
+        switch (choice) {
+        case 1:
+            cout << "Enter the value to push:";
+            cin >> value;
+            stack.push(value);  //Push the entered value onto the stack
+            break;
+        }
     }
 }
