2deac9c30aeec45e2f6b42f0c91dc7711d0ef426
Kuroyukimaru <oranggabut712@gmail.com>
2024-05-11 09:32:58 +0700

    Mengalokasikan memori untuk simpul baru

--- stuckusinglinkedlist_0151/stuckusinglinkedlist_0151.cpp
+++ stuckusinglinkedlist_0151/stuckusinglinkedlist_0151.cpp
@@ -25,6 +25,7 @@ public:
 
     // push operation: Insert an element onto the top of the stack
     int push(int value) {
+        Node* newNode = new Node(); // 1. Allocate memory for the new node
         
     }
 };
